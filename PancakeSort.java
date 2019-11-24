b0421f2746d84787c5a2d354d4e78e3af874ff18
Caijun Qin <47021574+Fennec2000GH@users.noreply.github.com>
2019-11-23 21:29:42 -0500

    List is replaced by Collection for parameter and return value

--- PancakeSort.java
+++ PancakeSort.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.List;
 
 public abstract class PancakeSort {
     //MEMBER FUNCTIONS
@@ -14,21 +15,21 @@ public abstract class PancakeSort {
     public static String getSortClass(){ return "Other"; }
 
     //MUTATORS
-    public static List<Number> sorted(List<Number> arr){
+    public static Collection<Number> sorted(Collection<Number> arr){
         if(arr == null)
             return null;
         List<Number> output = new ArrayList<>(arr);
         Number maximum = Collections.max(arr.stream().mapToDouble(Number::doubleValue).boxed().collect(Collectors.toList()));
-        int maxIndex = arr.lastIndexOf(maximum), endIndex = arr.size() - 1;
+        int maxIndex = output.lastIndexOf(maximum), endIndex = arr.size() - 1;
         while(endIndex > 0){
             Collections.reverse(output.subList(0, maxIndex + 1));
             Collections.reverse(output.subList(0, endIndex + 1));
             --endIndex;
-            maximum = Collections.max(arr.subList(0, endIndex + 1).stream().mapToDouble(Number::doubleValue).boxed().collect(Collectors.toList()));
-            maxIndex = arr.subList(0, endIndex + 1).lastIndexOf(maximum);
+            maximum = Collections.max(output.subList(0, endIndex + 1).stream().mapToDouble(Number::doubleValue).boxed().collect(Collectors.toList()));
+            maxIndex = output.subList(0, endIndex + 1).lastIndexOf(maximum);
         }
         return output;
     }
 
-    public static List<Number> sorted(Number[] arr){ return PancakeSort.sorted(Arrays.asList(arr)); }
+    public static Collection<Number> sorted(Number[] arr){ return PancakeSort.sorted(Arrays.asList(arr)); }
 }
